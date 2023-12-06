3ce6476213ddd754f71ca89f9ab629e9a4d5864d
Patrick <Patrick@Arbeits-Laptop1>
2023-12-06 15:26:11 +0100

    Initial commit

---
+++ src/main/java/de/othr/proconnect/ProConnectApplication.java
@@ -0,0 +1,13 @@
+package de.othr.proconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/othr/proconnect/ProConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.othr.proconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
